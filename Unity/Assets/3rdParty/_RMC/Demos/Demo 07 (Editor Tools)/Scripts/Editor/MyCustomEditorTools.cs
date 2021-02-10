b5a96f5a38c05942903f9addaecf232609f9bcc8
srivello <srivello@hotmail.com>
2021-02-10 10:49:17 -0600

    Fix Rendering & Input

--- Unity/Assets/3rdParty/_RMC/Demos/Demo 07 (Editor Tools)/Scripts/Editor/MyCustomEditorTools.cs
+++ Unity/Assets/3rdParty/_RMC/Demos/Demo 07 (Editor Tools)/Scripts/Editor/MyCustomEditorTools.cs
@@ -16,14 +16,16 @@ namespace RMC.IntroToUnity.Demos.EditorTools
 		//  Properties -----------------------------------
 
 		//  Fields ---------------------------------------
-		private const string MenuItemTitle = "Tools/Intro To Unity Course/Demos/EditorTools/Arrange Selected Scene Items";
-		
+		private const string MenuItemTitle01 = "Tools/Intro To Unity Course/Demos/EditorTools/Arrange Selected Scene Items";
+		private const string MenuItemTitle02 = "Tools/Intro To Unity Course/Demos/EditorTools/Randomize Selected Scene Items";
+
+
 		//  Initialization -------------------------------
 
 		//  Unity Methods   ------------------------------
 
 		//  Other Methods --------------------------------
-		[MenuItem(MenuItemTitle, validate = false, priority = 0)]
+		[MenuItem(MenuItemTitle01, validate = false, priority = 0)]
 		public static void ArrangeSelectedSceneItems()
 		{
 			Vector3 center = new Vector3(0, 0, 0);
@@ -36,13 +38,13 @@ namespace RMC.IntroToUnity.Demos.EditorTools
 
 			int i = 0;
 			for (int row = 0; row <= maxRows; row++)
-         {
+			{
 				for (int col = 0; col < maxCols; col++)
-            {
+				{
 					if (i >= maxItems)
-               {
+					{
 						break;
-               }
+					}
 
 					GameObject go = Selection.gameObjects[i];
 
@@ -52,15 +54,37 @@ namespace RMC.IntroToUnity.Demos.EditorTools
 
 					i++;
 				}
-         }
+			}
 		}
 
-		[MenuItem(MenuItemTitle, validate = true)]
+		[MenuItem(MenuItemTitle01, validate = true)]
 		public static bool ArrangeSelectedSceneItems_Validate()
 		{
 			return Selection.gameObjects.Length > 0;
 		}
-		
+
+		[MenuItem(MenuItemTitle02, validate = false, priority = 0)]
+		public static void RandomizeSelectedSceneItems()
+		{
+			float x = UnityEngine.Random.Range(-5f, 5f);
+			float y = UnityEngine.Random.Range(-5f, 5f);
+			float z = 0;
+
+			int maxItems = Selection.gameObjects.Length;
+
+			for (int i = 0; i < maxItems; i++)
+			{
+				GameObject go = Selection.gameObjects[i];
+				go.transform.position = new Vector3(x, y, z);
+			}
+		}
+
+		[MenuItem(MenuItemTitle02, validate = true)]
+		public static bool RandomizeSelectedSceneItems_Validate()
+		{
+			return Selection.gameObjects.Length > 0;
+		}
+
 		//  Event Handlers -------------------------------
 	}
 }
